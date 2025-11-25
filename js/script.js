0f8c23ad0421089e3ac79f558d46a602f9e3268c
Błażej Potasiński <blazej.potasinski@wp.pl>
2025-11-25 19:39:21 +0100

    add class active

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE]remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
@@ -12,9 +12,12 @@ for(let activeLink of activeLinks){
 }
 
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+
+  /* [DONE]remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('.post.active');
 
 for(let activeArticle of activeArticles){
