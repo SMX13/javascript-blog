54a029f77f6b5fc5c60765adb04ffe437ed31c97
Błażej Potasiński <blazej.potasinski@wp.pl>
2025-12-14 17:09:38 +0100

    zadanie 5.4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const optArticleSelector = '.post';
-const optTitleSelector = '.post-title';
-const optTitleListSelector = '.titles';
+
 
 function titleClickHandler(event){
   event.preventDefault();
@@ -36,20 +34,44 @@ function titleClickHandler(event){
   }
 }
 
+
+
+const optArticleSelector = '.post';
+const optTitleSelector = '.post-title';
+const optTitleListSelector = '.titles';
+
 function generateTitleLinks(){
+
+  /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
+  /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
 
+  /* for each article */
   for(let article of articles){
-    const articleId = article.getAttribute('id');
-    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-    const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+    const titleElement = article.querySelector(optTitleSelector);
+
+    /* get the title from the title element */
+    const articleTitle = titleElement.innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML =
+      '<li><a href="#' + articleId + '"><span>' +
+      articleTitle +
+      '</span></a></li>';
+
+    /* insert link into titleList */
     titleList.innerHTML += linkHTML;
   }
 
+  /* add click listeners to generated links */
   const links = document.querySelectorAll('.titles a');
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
